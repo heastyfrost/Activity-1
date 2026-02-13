f09a2ec3d09b7370acb9534cbf4a2699b9d9e917
heastyfrost <heastfrostel@gmail.com>
2026-02-13 10:30:37 +0800

    sentence_maker

---
+++ act 2/sentence maker.js
@@ -0,0 +1,25 @@
+let adjective = "fun";
+let noun = "Apex Legends";
+let verb = "many kills";
+let place = "toxic";
+let adjective2 = "skilled players";
+let noun2 = "Pro Gamers";
+
+let story = `I had a ${adjective} day playing ${noun}! I got ${verb} in a ${place} environment. However, I met some ${adjective2} who are aspiring ${noun2}. It was an unforgettable experience!`;
+
+let firstStory = story;
+
+console.log(story);
+console.log(firstStory);
+
+
+adjective = "amazing";
+noun = "Play style";
+verb = "learn";
+place = "enthusiastically";
+adjective2 = "cool";
+noun2 = "Games";
+
+story = `I had an ${adjective} day learning about ${noun2}! I can't wait to ${verb} what I've learned with others. I will do this ${place}. I found some ${adjective2} ${noun2} that I want to share!`;
+
+console.log(story);
